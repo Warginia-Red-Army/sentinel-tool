03d3dde27ebdfea73ba238e09765e0f960f6ee15
thief01 <pst12345@wp.pl>
2026-08-26 13:54:32 +0200

    test

--- fix_output.py
+++ fix_output.py
@@ -7,14 +7,15 @@ import shutil
 # base name = date + scene (final name)
 def fix_path(path, files, base_name):
     type = "rgb" if "rgb" in path else "bands"
-    final_path = f"output/{base_name}"
+    final_path = f"data/{base_name}"
     # fix files name
     for file in files:
         src = os.path.join(path, file)
         dst = f"{path}/{file}_{type}"
         os.replace(src,dst)
 
-    shutil.move(path, final_path)
+    shutil.copy(path, final_path)
 
     print(f"Moved from {path} to {dst}")
 
+fix_path("cache/raw/rgb/496ceeef5210f23ffff65483049f026c", ["request.json", "response.png"], "test_date")
