6dde9b66bb806b92b82dad64798240457c358aa9
thief01 <pst12345@wp.pl>
2026-08-26 14:06:00 +0200

    copy files

--- fix_output.py
+++ fix_output.py
@@ -9,17 +9,18 @@ from pathlib import Path
 def fix_path(path, files, base_name):
     type = "rgb" if "rgb" in path else "bands"
     final_path = f"data/raw/{base_name}"
+    os.makedirs(final_path, exist_ok=True)
     # fix files name
     for file in files:
         old_name = Path(file)
         src = os.path.join(path, file)
         new_name = old_name.with_stem(old_name.stem + f"_{type}")
         dst = f"{path}/{new_name}"
-        print(dst)
-        os.replace(src,dst)
+        # print(dst)
+        # os.replace(src,dst)
+        shutil.copy(src, dst)
 
-    os.makedirs(final_path, exist_ok=True)
-    shutil.copy(path, final_path)
+
 
     print(f"Moved from {path} to {final_path}")
 
