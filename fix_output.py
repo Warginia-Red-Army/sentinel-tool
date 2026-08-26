f5ccc0dd9b6a6df62fe5d6b5770dc9884532e818
thief01 <pst12345@wp.pl>
2026-08-26 14:02:28 +0200

    fix dst

--- fix_output.py
+++ fix_output.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+from pathlib import Path
 
 
 # path =cache/raw | rgb/fgawefawjiowfe
@@ -7,15 +8,18 @@ import shutil
 # base name = date + scene (final name)
 def fix_path(path, files, base_name):
     type = "rgb" if "rgb" in path else "bands"
-    final_path = f"data/{base_name}"
+    final_path = f"data/raw/{base_name}"
     # fix files name
     for file in files:
+        old_name = Path(file)
         src = os.path.join(path, file)
-        dst = f"{path}/{file}_{type}"
+        new_name = old_name.with_stem(old_name.stem + f"_{type}")
+        dst = f"{path}/{new_name}"
+        print(dst)
         os.replace(src,dst)
 
-    shutil.copy(path, final_path)
+    # shutil.copy(path, final_path)
 
-    print(f"Moved from {path} to {dst}")
+    print(f"Moved from {path} to {final_path}")
 
 fix_path("cache/raw/rgb/496ceeef5210f23ffff65483049f026c", ["request.json", "response.png"], "test_date")
