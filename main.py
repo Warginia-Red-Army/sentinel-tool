f6ffd28c73dcdc790ede328dcc3c449da0e92ae1
thief01 <pst12345@wp.pl>
2026-09-02 08:10:15 +0200

    add db

--- main.py
+++ main.py
@@ -2,11 +2,25 @@ import argparse
 import calendar
 import os
 from datetime import date
+import sqlite3
 
 from sentinelhub import (
     CRS,
     BBox
 )
+
+connection = sqlite3.connect("sentinelhub.db")
+cursor = connection.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS locations (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    latitude1 REAL NOT NULL,
+    longitude1 REAL NOT NULL,
+    latitude2 REAL NOT NULL,
+    longitude2 REAL NOT NULL,
+)""")
 
 import bbox_helper
 import download_map
