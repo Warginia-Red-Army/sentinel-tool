264b9e5c26222bf5929cf5399aee66c54985e973
thief01 <pst12345@wp.pl>
2026-09-04 16:17:55 +0200

    change download logic

--- cli/download.py
+++ cli/download.py
@@ -7,7 +7,9 @@ from sentinelhub import (
     BBox
 )
 
+from database.locations import get_location
 from tools import download_map
+from utility import bbox_helper
 
 
 def register_parser(tools):
@@ -16,7 +18,7 @@ def register_parser(tools):
 
     parser.add_argument('-l', "--location", required=True)
 
-    parser.set_defaults(func=download)
+    parser.set_defaults(func=download_input)
 
 # downlaod logic
 # since year
@@ -36,19 +38,30 @@ def process_month():
 def process_day():
     pass
 
+def download_input(args):
+    location = args.location
+    if not location:
+        location = input("Location: ")
+    start_year = input("Start year: ")
+    end_year = input("End year: ")
+    start_month = input("Start month: ")
+    end_month = input("End month: ")
+
+    coordinates = get_location(location)
+    bbox = BBox(bbox_helper.concert_to_bbox(coordinates), crs=CRS.WGS84)
+
+    download(start_year, end_year, start_month, end_month, bbox)
 
 
-def download():
+def download(start_year, end_year, start_month, end_month, bbox):
     today = date.today()
-    end_year = today.year
-
     os.makedirs("output", exist_ok=True)
 
-    for year in range(end_year - 10, end_year + 1):
-        for month in range(5, 9):
+    for year in range(start_year, end_year):
+        for month in range(start_month, end_month):
             month_start = date(year, month, 1)
             if month_start > today:
-                continue  # skip future months
+                continue  # skip future monthssa
 
             last_day = calendar.monthrange(year, month)[1]
             month_end = date(year, month, last_day)
@@ -56,6 +69,4 @@ def download():
                 month_end = today
 
             time_interval = (month_start.isoformat(), month_end.isoformat())
-
-            bbox = BBox(bbox_helper.convertToCorrectBbox(bbox_helper.osielsko), crs=CRS.WGS84)
             download_map.download(time_interval, bbox)
